ae5c0a2a4e725b89893d4b38de03d3e9f34008cb
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2025-02-16 20:43:07 +0100

    Finished frontend

--- app/frontend/src/components/home/home.tsx
+++ app/frontend/src/components/home/home.tsx
@@ -3,55 +3,79 @@ import React, {FC, JSX, useEffect, useState} from "react";
 import {converTifImagePost} from "../../services/convert_tif";
 import {modelApiPost} from "../../services/model_api";
 import DropZone from "../dropzone/dropzone";
-import Gallery from "../gallery/gallery";
 import "./home.css";
 
 const Home: FC = (): JSX.Element => {
     const [zipFile, setFile] = useState<JSZip | null>(null);
-    const selectorMRI = "MRI";
-    const selectorMask = "Mask";
-    const selectorResults = "Results";
+    const [urls, setUrls] = useState<any[]>([]);
 
-    useEffect(() => {
+    const maskCancerShow = async () => {
         if (!!zipFile) {
+            const buff_urls: any[] = [];
             for (let filename of Object.keys(zipFile.files).sort()) {
-                zipFile.files[filename].async("blob").then(async (blob) => {
-                    const data = await converTifImagePost(blob);
-                    if (!!data) {
-                        const img = new Image();
-                        img.src = URL.createObjectURL(data);
-                        let selector = "#" + (filename.includes("mask") ? selectorMask : selectorMRI);
-                        const elem = document.querySelector(selector);
-                        if (!!elem) elem.prepend(img);
+                if (zipFile.files[filename].dir) {
+                    continue;
+                }
+                const blob = await zipFile.files[filename].async("blob");
+                const data = await converTifImagePost(blob);
+                if (!!data) {
+                    const img_url = URL.createObjectURL(data);
 
-                        if (selector === "#" + selectorMRI) {
-                            const data = await modelApiPost(blob);
-                            if (!!data) {
-                                const img = new Image();
-                                img.src = URL.createObjectURL(data);
-                                selector = "#" + selectorResults;
-
-                                const elem = document.querySelector(selector);
-                                if (!!elem) elem.prepend(img);
-                            }
+                    if (!filename.includes("mask")) {
+                        const data = await modelApiPost(blob);
+                        if (!!data) {
+                            const result_url = URL.createObjectURL(data);
+                            buff_urls.push({
+                                filename: filename.substring(0, filename.length - 4),
+                                img: img_url,
+                                result: result_url,
+                            });
                         }
+                    } else {
+                        const maskName = filename.substring(0, filename.length - 9);
+                        const obj = buff_urls.find((x) => x.filename === maskName);
+                        obj["mask"] = img_url;
                     }
-                });
+                }
             }
+            setUrls(buff_urls);
         }
+    };
+    useEffect(() => {
+        maskCancerShow();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [zipFile]);
 
     return (
         <div className="header">
-            {!zipFile ? (
-                <DropZone {...{setFile: setFile}} />
-            ) : (
-                <div>
-                    <Gallery selector={selectorMRI} />
-                    <Gallery selector={selectorMask} />
-                    <Gallery selector={selectorResults} />
-                </div>
-            )}
+            <DropZone {...{setFile: setFile}} />
+
+            <div>
+                <hr />
+                {!!urls &&
+                    urls.map((obj, index) => {
+                        return (
+                            <div>
+                                <h5>{obj.filename}</h5>
+                                <div key={index} className="float-container">
+                                    <div className="float-child">
+                                        <img src={obj.img} alt={obj.img} />
+                                        <p>Image</p>
+                                    </div>
+                                    <div className="float-child">
+                                        <img src={obj.mask} alt={obj.mask} />
+                                        <p>Mask</p>
+                                    </div>
+                                    <div className="float-child">
+                                        <img src={obj.result} alt={obj.result} />
+                                        <p>Result</p>
+                                    </div>
+                                </div>
+                                <hr />
+                            </div>
+                        );
+                    })}
+            </div>
         </div>
     );
 };
